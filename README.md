193c5c2205654f0a9d33e2ec6cdc48f2142571cd
Joshua Castor <jcasto3@uic.edu>
2018-02-15 07:46:16 -0600

    Added ability to specify a public root folder

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## Usage
 1. Run `npm install`
 2. Start MongoDB Server.
-2. Run `npm run server`
+2. Run `npm run server`.
+  * If your build isn't located at `..\coliberate-frontend\dist`, you can specify the exact location with `npm run server -- -f <path to build folder>`
--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const express = require('express'),
   app = express();
 
 const argv = require('yargs')
-  .usage('Usage: $0 -p [integer] -i [string of IP address]')
+  .usage('Usage: $0 -p [integer] -i [string of IP address] -f [directory to build or dist folder]')
   .default("p", 80)
   .default("i", '127.0.0.1')
+  .default("f", `${__dirname}/public`)
   .alias('p', 'port')
   .alias('i', 'ip').alias('i', 'ip-address')
+  .alias('f','build-folder').alias('f', 'dist-folder')
   .describe('p', 'Port to run server on')
   .describe('i', 'IP Address to run server on')
-  .help('h')
-  .alias('h', 'help')
+  .describe('f', 'Directory to use as the public root folder (i.e. accessible via URL). Must be an absolute path that doesn\'t end with a slash')
+  .help('h').alias('h', 'help')
   .argv;
 
 // files in public folder can be accessed via URL
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(argv['build-folder']));
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html');
+  res.sendFile(`./index.html`, { root: argv['build-folder'] });
 });
 
 app.get('/data', (req, res) => {
@@ -32,5 +34,6 @@ if (argv.ip !== '127.0.0.1') {
 }
 
 function onServerReady() {
+  console.info(`Using ${argv['build-folder']} as public root folder`);
   console.info("Listening on " + this.address().address + ":" + this.address().port);
 }
