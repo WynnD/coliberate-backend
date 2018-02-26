756385d01c84592d3db326c8d4fef82d527d3f5e
Joshua Castor <jcasto3@uic.edu>
2018-02-26 10:50:43 -0600

    Added test for member add/removal

--- ColiberateDbWrapper.js
+++ ColiberateDbWrapper.js
@@ -73,12 +73,52 @@ class ColiberateDbWrapper {
     });
   }
 
+  deleteInDB(collectionName, query) {
+    return new Promise((fulfill, reject) => {
+      if (!query) {
+        reject(Error('No query defined for dropMember'));
+      }
+      this.getDatabaseInstance()
+        .then(db => {
+          db.collection(collectionName)
+            .deleteOne(query, (err, result) => {
+              if (err) {
+                reject(err);
+              } else {
+                fulfill(result);
+              }
+            });
+        }).catch(reject);
+    });
+  }
+
+  dropCollectionInDB(collectionName) {
+    return new Promise((fulfill, reject) => {
+      this.getDatabaseInstance()
+        .then(db => {
+          db.collection(collectionName)
+            .drop((err, delOK) => {
+              if (err) {
+                reject(err);
+              } else {
+                fulfill(delOK);
+              }
+            });
+        }).catch(reject);
+    });
+  }
+
   // checks for valid fields
   isValidMember(member) {
     if (typeof member !== 'object') {
       return false;
-    } else if (!member.username || !member.password || !member.id || !member.name) {
-      return false;
+    } else {
+      const expectedFields = ['username', 'password', 'id', 'name', 'email', 'name'];
+      const hasAMissingField = expectedFields.filter(f => !member[f]).length > 0;
+      
+      if (hasAMissingField) {
+        return false;
+      }
     }
 
     return true;
@@ -93,14 +133,23 @@ class ColiberateDbWrapper {
     }
   }
 
-  async searchMembers(query = {}, fieldsToExclude = {}) {
+  async deleteMember(query) {
+    return await this.deleteInDB('members', query);
+  }
+
+  async findMember(query) {
+    return await this.findInDB('members', query);
+  }
+
+  async searchMembers(query = {}, fieldsToExclude = { password: 0 }) {
     return await this.findInDB('members', query, fieldsToExclude);
   }
 
-  async closeConnectionToDatabase() {
+  async closeConnection() {
     const dbInstance = await this.getDatabaseInstance();
     dbInstance.close();
     this.dbInstance = null;
+    this.connectInstance = null;
   }
 }
 
--- coliberateDbWrapper.test.js
+++ coliberateDbWrapper.test.js
@@ -17,3 +17,38 @@ test('connects to coliberate database in mongo', async () => {
   expect(dbInstance).toBeTruthy();
   expect(dbInstance.s.databaseName).toBe('coliberate');
 });
+
+test('adding and removing a member in the database', async () => {
+  const db = new dbWrapper(url);
+  const member = {
+    id: 5,
+    name: 'Test Member',
+    email: 'Test@example.com',
+    password: 'password',
+    username: 'Test@example.com'
+  };
+  // # of keys + 1 for _id field added by mongo
+  expect.assertions(1 + Object.keys(member).length + 1 + 5);
+
+  console.log("TODO: Refactor into a global setup/teardown method");
+  await db.dropCollectionInDB('members');
+
+  await db.addMember(member);
+
+  let searchResult = await db.findMember({ id: member.id });
+  expect(searchResult.length).toBe(1);
+  // expect result to be equal to inserted member
+  Object.keys(member).forEach(key => {
+    expect(searchResult[0][key]).toEqual(member[key]);
+  });
+
+  const deleteResult = await db.deleteMember({ id: member.id });
+  expect(deleteResult).toBeTruthy();
+  expect(deleteResult.result).toBeDefined();
+  // expect result to be equal to inserted member
+  expect(deleteResult.result.n).toEqual(1);
+  expect(deleteResult.result.ok).toEqual(1);
+  
+  searchResult = await db.findMember({ id: member.id });
+  expect(searchResult.length).toBe(0);
+});
